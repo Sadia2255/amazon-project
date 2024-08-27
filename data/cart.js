4c96dd9ff9ad5edb2863436e3f077e77c07663c7
Sadia2255 <Sadia22551@gmail.com>
2024-08-26 22:46:07 -0400

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -51,6 +51,19 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
+
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
