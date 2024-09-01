9271b655cfee22e3888d0e60237ad990ecfa3342
Sadia2255 <Sadia22551@gmail.com>
2024-09-01 01:00:39 -0400

    Created the update functionality

--- data/cart.js
+++ data/cart.js
@@ -49,6 +49,20 @@ export function addToCart(productId) {
   saveToStorage();
 }
 
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
+
 export function removeFromCart(productId) {
   const newCart = [];
   cart.forEach((cartItem) => {
